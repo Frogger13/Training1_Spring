04d87cbea10a11310853522ce085b715fae8af6a
Frogger13 <gfg071298@yandex.ru>
2020-12-05 13:43:35 +0300

    Creating bean

---
+++ src/main/java/org/example/TeastBean.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class TeastBean {
+    private String name;
+
+    public TeastBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
